a15ec91713c56606625ad2f0331c562199eb028b
Philippe Perret <philippe.perret@yahoo.fr>
2020-08-04 20:57:43 +0200

    Ajout des locutions, prêtes à être traitées

--- lib/required/_first/constants/proximites.rb
+++ lib/required/_first/constants/proximites.rb
@@ -14,3 +14,15 @@ MOTS_TIRETS = {
   'peut-être'     => true,
   'grand-chose'   => true
 }
+
+LOCUTIONS = {
+  # loc:  La locution auquel peut appartenir le mot. Si elle est trouvée,
+  #       aucun proximité n'est signalée.
+  # req:  Indique comment il faut prendre les mots autour pour obtenir la
+  #       locution. Par exemple, [-1, 2] signifie qu'il faut prendre le
+  #       mot avant et les deux mots suivants, tandis que [-3,nil] signifie
+  #       qu'il faut prendre les 3 mots avant et aucun mot après.
+  #
+  # Toutes les locutions avec le mot "temps"
+  'temps' => {loc:'de temps en temps', req: [[-1,2], [-3,nil]]},
+}
